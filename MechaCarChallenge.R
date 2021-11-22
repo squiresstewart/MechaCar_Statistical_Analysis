5a3abfcf254d6160577044c6e25049bd4535c1d6
Daniel Squires <danielsquires@Daniels-MacBook-Pro.local>
2021-11-22 12:00:39 -0600

    done

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,7 +2,11 @@ library(tidyverse)
 
 demo_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=demo_table)
+
 Mecha_car_model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=demo_table)
+
+summary(lm(Mecha_car_model))
 
 Mecha_sum <- summary(lm(Mecha_car_model))
 
